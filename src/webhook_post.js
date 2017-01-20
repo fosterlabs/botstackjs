d6a44175fb9d03fd362093248d16aa82d7f996e2
Alex Sazonov <sazonov@devlab.su>
2017-01-20 21:54:07 +0300

    webhook_post.js refactor

--- src/webhook_post.js
+++ src/webhook_post.js
@@ -1,10 +1,10 @@
-﻿var fb = require("./fb");
-var apiai = require("./api-ai.js");
-var botmetrics = require('./bot-metrics.js');
-var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).facebook;
-var sessionStore = require('./session.js');
+﻿let fb = require("./fb");
+let apiai = require("./api-ai.js");
+let botmetrics = require('./bot-metrics.js');
+let dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).facebook;
+let sessionStore = require('./session.js');
 
-module.exports = function (req, res, next) {
+module.exports = (req, res, next) => {
     console.log(" ");
     console.log("===Received a message from FB");
 
@@ -13,32 +13,30 @@ module.exports = function (req, res, next) {
 
     console.log('JSON:' + JSON.stringify(req.body));
 
-    var entries = req.body.entry;
-    entries.forEach(function (entry) {
-        var messages = entry.messaging;
-        messages.forEach(function (message) {
-            var senderID = message.sender.id;
+    let entries = req.body.entry;
+    entries.forEach((entry) => {
+        let messages = entry.messaging;
+        messages.forEach((message) => {
+            let senderID = message.sender.id;
             //create or update the session for this user
             sessionStore.set(senderID);
 
             // check if it is a text message
-            var isTextMessage = message.message.text ? true : false;
+            let isTextMessage = message.message.text ? true : false;
             if (isTextMessage) {
                 // get the biz from api.ai
-                var text = message.message.text;
+                let text = message.message.text;
                 // check for an instagram url
-                
+
                 //pass to api.ai
                 console.log('Sending to A   PI.ai:' + text + ' Sender:' + senderID);
 
-                apiai.processTextMessage(text, senderID).then(function (apiaiResp) {
+                apiai.processTextMessage(text, senderID).then((apiaiResp) => {
                     fb.processMessagesFromApiAi(apiaiResp, senderID);
-
-                }, function (err) {
+                }, (err) => {
                     fb.reply(fb.textMessage("I'm sorry, I didn't understand that"), senderID);
                     console.log("Error in api.ai response:", err);
                 });
-                
             } else {
                 console.log("not a text message");
                 console.log(message.attachments);
