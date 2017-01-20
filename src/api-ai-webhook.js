793e4ec34fbb060dfe03d8e11b1689c8a8a124a7
Alex Sazonov <sazonov@devlab.su>
2017-01-20 21:38:32 +0300

    api-ai-webhook refactor

--- src/api-ai-webhook.js
+++ src/api-ai-webhook.js
@@ -1,7 +1,7 @@
-var db = require('./dynamodb.js');
+const db = require('./dynamodb.js');
 
-module.exports = function (req, res, next) {
-	
+module.exports = (req, res, next) => {
+
 	res.json({
 		speech: req.body.result.fulfillment.speech,
 		displayText: req.body.result.fulfillment.speech,
@@ -10,13 +10,13 @@ module.exports = function (req, res, next) {
 
 	res.end();
 	console.log("===Received a database hook from API.ai");
-	
+
     //add to db
     if(req.body) {
         db.logApiaiObject(req.body);
     } else {
         console.log('No body to put in DB');
     }
-	
+
 	return next();
 };
