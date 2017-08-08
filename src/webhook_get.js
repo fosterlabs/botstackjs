3ca3600f1e79c894dbcc2c92df4a070d878f4a2d
webus <webusnix@gmail.com>
2017-08-08 14:38:58 +0300

    fixed by eslint

--- src/webhook_get.js
+++ src/webhook_get.js
@@ -1,13 +1,13 @@
-﻿//The endpoint for facebook to verify the bot
+// The endpoint for facebook to verify the bot
 
 module.exports = (req, res, next) => {
-    let token = req.query.hub.verify_token;
-    if (token === process.env.FB_VERIFY_TOKEN) {
-        res.write(req.query.hub.challenge);
-        res.end();
-    } else {
-        res.send("Error, wrong validation token");
-    }
+  const token = req.query.hub.verify_token;
+  if (token === process.env.FB_VERIFY_TOKEN) {
+    res.write(req.query.hub.challenge);
+    res.end();
+  } else {
+    res.send('Error, wrong validation token');
+  }
 
-    return next();
+  return next();
 };
--- src/webhook_post.js
+++ src/webhook_post.js
@@ -1,56 +1,56 @@
-﻿const fb = require("./fb");
-const apiai = require("./api-ai.js");
+const fb = require('./fb');
+const apiai = require('./api-ai.js');
 const botmetrics = require('./bot-metrics.js');
 const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).facebook;
 const sessionStore = require('./session.js')();
 const log = require('./log.js');
 
 function processWelcomeMessage(messageText, senderID) {
-    
+
 }
 
 module.exports = (req, res, next) => {
-    console.log(" ");
-    console.log("===Received a message from FB");
+  console.log(' ');
+  console.log('===Received a message from FB');
 
-    res.end();
-    dashbot.logIncoming(req.body);
+  res.end();
+  dashbot.logIncoming(req.body);
 
-    console.log('JSON:' + JSON.stringify(req.body));
+  console.log(`JSON:${JSON.stringify(req.body)}`);
 
-    let entries = req.body.entry;
-    entries.forEach((entry) => {
-        let messages = entry.messaging;
-        messages.forEach((message) => {
-            let senderID = message.sender.id;
-            //create or update the session for this user
-            sessionStore.set(senderID);
+  const entries = req.body.entry;
+  entries.forEach((entry) => {
+    const messages = entry.messaging;
+    messages.forEach((message) => {
+      const senderID = message.sender.id;
+            // create or update the session for this user
+      sessionStore.set(senderID);
 
             // check if it is a text message
-            let isTextMessage = message.message.text ? true : false;
-            if (isTextMessage) {
+      const isTextMessage = !!message.message.text;
+      if (isTextMessage) {
                 // get the biz from api.ai
-                let text = message.message.text;
+        const text = message.message.text;
                 // check for an instagram url
 
-                //pass to api.ai
-                console.log('Sending to API.ai:' + text + ' Sender:' + senderID);
+                // pass to api.ai
+        console.log(`Sending to API.ai:${text} Sender:${senderID}`);
 
-                apiai.processTextMessage(text, senderID).then((apiaiResp) => {
-                    fb.processMessagesFromApiAi(apiaiResp, senderID);
-                }, (err) => {
-                    fb.reply(fb.textMessage("I'm sorry, I didn't understand that"), senderID);
-                    console.log("Error in api.ai response:", err);
-                });
-            } else {
-                console.log("not a text message");
-                console.log(message.attachments);
-            }
+        apiai.processTextMessage(text, senderID).then((apiaiResp) => {
+          fb.processMessagesFromApiAi(apiaiResp, senderID);
+        }, (err) => {
+          fb.reply(fb.textMessage("I'm sorry, I didn't understand that"), senderID);
+          console.log('Error in api.ai response:', err);
         });
+      } else {
+        console.log('not a text message');
+        console.log(message.attachments);
+      }
     });
+  });
 
-    console.log("===End of FB message");
-    console.log("===");
-    return next();
+  console.log('===End of FB message');
+  console.log('===');
+  return next();
 };
 
