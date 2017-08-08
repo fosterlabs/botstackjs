60415c7cf9d03d794502a7063559c58ed5267f24
webus <webusnix@gmail.com>
2017-08-08 14:55:50 +0300

    fixed by eslint

--- src/bot-metrics.js
+++ src/bot-metrics.js
@@ -1,25 +1,26 @@
-﻿const BOTMETRICS_TOKEN = process.env.BOTMETRICS_TOKEN;
+const BOTMETRICS_TOKEN = process.env.BOTMETRICS_TOKEN;
 
 const BotMetrics = require('node-botmetrics');
-let botmetrics = new BotMetrics(BOTMETRICS_TOKEN);
+
+const botmetrics = new BotMetrics(BOTMETRICS_TOKEN);
 
 function logUserRequest(message, conversationId) {
-	log(message, conversationId, conversationId, 'incoming');
-};
+  log(message, conversationId, conversationId, 'incoming');
+}
 
 function logServerResponse(message, conversationId) {
-	log(message, conversationId, conversationId, 'outgoing');
-};
+  log(message, conversationId, conversationId, 'outgoing');
+}
 
 function log(message, conversationId, userId, type) {
-	botmetrics.track({
-		text: message,
-		message_type: type,
-		user_id: userId,
-		conversation_id: conversationId,
-		platform: 'messenger'
-	});
-};
+  botmetrics.track({
+    text: message,
+    message_type: type,
+    user_id: userId,
+    conversation_id: conversationId,
+    platform: 'messenger'
+  });
+}
 
 exports.logUserRequest = logUserRequest;
 exports.logServerResponse = logServerResponse;
--- src/dashbot.js
+++ src/dashbot.js
@@ -3,13 +3,12 @@ const proxy = require('proxy-method-missing');
 
 let dashbot = null;
 if ('DASHBOT_API_KEY' in process.env) {
-    dashbot = require('dashbot')(process.env.DASHBOT_API_KEY)
+  dashbot = require('dashbot')(process.env.DASHBOT_API_KEY);
 }
 
-module.exports = function(provider) {
-    if (dashbot) {
-        return dashbot[provider];
-    } else {
-        return proxy({}, (method, ...args) => { return null; });
-    }
+module.exports = function (provider) {
+  if (dashbot) {
+    return dashbot[provider];
+  }
+  return proxy({}, (method, ...args) => null);
 };
