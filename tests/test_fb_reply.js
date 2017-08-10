7a7fa7c63cb737a5aa463887c5a3b90e9303777b
webus <webusnix@gmail.com>
2017-08-10 13:25:06 +0300

    testing fb reply

---
+++ tests/test_fb_reply.js
@@ -0,0 +1,39 @@
+const lodash = require('lodash');
+const rewiremock = require('rewiremock').default;
+const { addPlugin, plugins } = require('rewiremock');
+
+addPlugin(plugins.relative);
+addPlugin(plugins.usedByDefault);
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const assert = chai.assert;
+
+describe('Testing FB reply', () => {
+  it('Test One', async () => {
+
+    let rpData = null;
+
+    rewiremock('request-promise').with(async (data) => {
+      rpData = data;
+      return null;
+    });
+
+    rewiremock.enable();
+    rewiremock.isolation();
+
+    const fb = require(rewiremock.resolve('../src/fb/reply'));
+    const fbMsg = require(rewiremock.resolve('../src/fb/message_types'));
+    const senderID = '1234567890';
+    await fb.reply(fbMsg.textMessage('hello'), senderID);
+    console.log(rpData);
+    assert.equal(lodash.get(rpData, 'method'), 'POST');
+    assert.equal(lodash.get(rpData, 'json.recipient.id'), senderID);
+    assert.equal(lodash.get(rpData, 'json.message.text'), 'hello');
+    assert.isOk(lodash.has(rpData, 'qs.access_token'));
+    assert.isOk(lodash.has(rpData, 'url'));
+  });
+});
