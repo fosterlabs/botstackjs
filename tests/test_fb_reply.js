8d09141e8d638385ff28e7e173641995a10c80b0
webus <webusnix@gmail.com>
2017-08-15 10:40:42 +0300

    fixed by eslint

--- tests/test_fb_reply.js
+++ tests/test_fb_reply.js
@@ -14,7 +14,6 @@ const assert = chai.assert;
 
 describe('Testing FB reply', () => {
   it('fb reply', async () => {
-
     let rpData = null;
 
     rewiremock('request-promise').with(async (data) => {
